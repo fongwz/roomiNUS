9c03e50b611e0615334c912e838fe51ae8bb5a8c
fongwz <e0035221@u.nus.edu>
2018-11-13 17:14:48 +0800

    Updates

---
+++ precache-manifest.cd84e24576aa45c60e1518967338be1c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/roomiNUS/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "52c10ea5d0deae6cffe5",
+    "url": "/roomiNUS/static/css/main.074ee3bc.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/roomiNUS/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "522cf71502bc59c8a40f",
+    "url": "/roomiNUS/static/js/1.522cf715.chunk.js"
+  },
+  {
+    "revision": "3340172a6a5497dffc0a",
+    "url": "/roomiNUS/static/js/runtime~main.3340172a.js"
+  },
+  {
+    "revision": "112b90a823b9723990adb069a33d7104",
+    "url": "/roomiNUS/static/media/Elronmonospace.112b90a8.ttf"
+  },
+  {
+    "revision": "09f6b2ba02572f19a7d7dd2b5f6494ad",
+    "url": "/roomiNUS/static/media/CenturyGothic.09f6b2ba.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/roomiNUS/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/roomiNUS/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/roomiNUS/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/roomiNUS/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/roomiNUS/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "52c10ea5d0deae6cffe5",
+    "url": "/roomiNUS/static/js/main.52c10ea5.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/roomiNUS/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/roomiNUS/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/roomiNUS/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/roomiNUS/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/roomiNUS/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/roomiNUS/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/roomiNUS/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/roomiNUS/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/roomiNUS/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "522cf71502bc59c8a40f",
+    "url": "/roomiNUS/static/css/1.a2764fb6.chunk.css"
+  },
+  {
+    "revision": "d16e94472139eb4f1269a82b4659da2d",
+    "url": "/roomiNUS/index.html"
+  }
+];
